a519a66617dbafc59800d8c47eb9427017cf9014
mvits <stivmarquez.ud@gmail.com>
2023-06-05 06:33:40 -0500

    Example Migration

--- my-project/database/migrations/2023_06_02_131008_create_score_table.php
+++ my-project/database/migrations/2023_06_02_131008_create_score_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('score', function (Blueprint $table) {
+            $table->increments("id");
+            $table->string("name");
+            $table->string("score");
+            $table->string("date");
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('score');
     }
 };
--- my-project/database/migrations/2023_06_02_133112_create_calificacion_table.php
+++ my-project/database/migrations/2023_06_02_133112_create_calificacion_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('calificacion', function (Blueprint $table) {
+            $table->id();
+            $table->string("estudiante");
+            $table->string("unidad");
+            $table->string("nota");
+            $table->timestamps();
         });
     }
 
@@ -23,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('calificacion');
     }
 };
